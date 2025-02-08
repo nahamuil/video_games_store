0b72458e9f8949fdf89d1f1e155e5af1fc5f5374
nahamu_il <ofek@nahamu.com>
2025-02-08 18:48:36 +0200

    fixed user class

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,12 +1,20 @@
-# create a __init__.py file in the models folder to make all the model act as a package if you dont have it already , also you can make one file with all the models just copy ythe all the code in the files into it
-
 from . import db
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    phone_number = db.Column(db.String(10), nullable=False)
-    city = db.Column(db.String(50), nullable=False)
-    age = db.Column(db.Integer, nullable=True)
-    # loan_id = db.Column()
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(120))
+    phone_number = db.Column(db.String(20))
+    loans = db.relationship('Loan', backref='user', lazy=True)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'name': self.name,
+            'email': self.email,
+            'phone_number': self.phone_number
+        }
